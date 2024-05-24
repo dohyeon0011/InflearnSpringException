ccea1f09d7367ffaa3df684eb348901d0bd365c3
idohyeon <ehgus5026@naver.com>
2024-05-25 01:45:57 +0900

    -@ResponseStatus

--- src/main/java/com/example/exception/userexception/BadRequestException.java
+++ src/main/java/com/example/exception/userexception/BadRequestException.java
@@ -5,4 +5,5 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "error.bad") // reason = "" 직접 입력 가능
 public class BadRequestException extends RuntimeException {
+
 }
