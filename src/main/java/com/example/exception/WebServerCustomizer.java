9b54cc0feabb92c7be4003acb2049b81d682cc6a
idohyeon <ehgus5026@naver.com>
2024-05-24 22:54:48 +0900

    -Error Controller Setting

--- src/main/java/com/example/exception/WebServerCustomizer.java
+++ src/main/java/com/example/exception/WebServerCustomizer.java
@@ -6,13 +6,13 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-//@Component 스프링 부트가 기본으로 제공하는 기본 오류 페이지 설정을 사용하기 위해 주석(스프링 부트는 기본으로 /error로 기본 경로 설정)
+//@Component // 스프링 부트가 기본으로 제공하는 기본 오류 페이지 설정을 사용하기 위해 주석(스프링 부트는 기본으로 /error로 기본 경로 설정)
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
 
-        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");   // Controller의 @RequestMapping("") 경로
         ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
         // 500 예외가 서버 내부에 발생한 오류라는 뜻을 포함하고 있기 때문에 여기서는 예외가 발생한 경우도 500 오류 화면으로 처리
         ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
