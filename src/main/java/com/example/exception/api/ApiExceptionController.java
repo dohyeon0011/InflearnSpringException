0a082d0c9174e76f37112f7743558d5fe0d0e8b4
idohyeon <ehgus5026@naver.com>
2024-05-25 04:09:07 +0900

    -ApiExceptionController(extendHandlerExceptionResolvers) + (ResponseExceptionResolver, DefaultHandlerExceptionResolver)

--- src/main/java/com/example/exception/api/ApiExceptionController.java
+++ src/main/java/com/example/exception/api/ApiExceptionController.java
@@ -1,12 +1,16 @@
 package com.example.exception.api;
 
+import com.example.exception.userexception.BadRequestException;
 import com.example.exception.userexception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -26,6 +30,24 @@ public class ApiExceptionController {
 
         return new MemberDto(id, "hello api exception :  " + id);
     }
+
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
+    // ResponseExceptionResolver -> HTTP 응답 코드 변경
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException()); // exception 직접 넣기
+    }
+
+    // DefaultHandlerExceptionResolver -> 스프링 내부 예외 처리
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        return "ok"; // 원래라면 자료형을 다르게 입력했을 때 TypeMissMatch가 떠서 was에 그대로 넘어가서 500에러가 떠야 하는데 스프링에서 자동으로 클라이언트 실수는 400으로 바꿔줌
+    }
+
 
     @Data
     @AllArgsConstructor
